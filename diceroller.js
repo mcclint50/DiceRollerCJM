5cd346adcd241ae79c034d4278a1189a6c198515
coli mcclinti <colinm610@gmail.com>
2020-09-27 12:41:20 -0500

    Final commit of diceroller.js

--- diceroller.js
+++ diceroller.js
@@ -5,7 +5,9 @@ function RollDice() {
     document.getElementById("d1").value = Math.floor(Math.random()*6) + 1;
     document.getElementById("d2").value = Math.floor(Math.random()*6) + 1;
     CheckDoubles(document.getElementById("d1").value, document.getElementById("d2").value);
-    document.getElementById("result").value = document.getElementById("d1").value + document.getElementById("d2").value;
+    var d1 = Number(document.getElementById("d1").value);
+    var d2 = Number(document.getElementById("d2").value);
+    document.getElementById("result").value = d1 + d2
 }
 
 //Only displays doubles message if d1 and d2 are equal
